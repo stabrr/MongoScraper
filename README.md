5b26534f05be9dd584d4301f811e4acbd687c659
stabrr <bobbystabile@yahoo.com>
2018-01-28 16:36:00 -0600

    read me update

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # MongoScraper
 use MongoDB to save links from scrapping website. Homework for NU Coding bootcamp
+
+MongoDB and Cheerio Scrapper
+
+Synopsis
+
+This project is a homework assignment working with MongoDB and Cheerio to scrap the NYtimes and list article and save them.  It should also be able to delete them but was not able to output the save articles so I could add the delete button.  Also supposed to add notes but did not get there.
+
+
+Motivation
+
+This is a homework assginment for Northwestern University Coding boot camp to understand how MongoDB and Cheerio works.  I have a pretty good understand of both but still struggle with incorporating the front with the back end.  I can do front end work and backend work but really struggle to send the backend data back to the front.
+
+
+Refer to package.json for packages used
+
+
+Click to save and buttons on top will scrape for more articles and click saved articles for the saved articles.
+
+Contributors
+
+Northwestern University Boot Camp
+Bob Stabile
+
--- server.js
+++ server.js
@@ -117,30 +117,31 @@ app.get("/saved", function(req, res) {
     // Or send the doc to the browser as a json object
     else {
       res.json(doc);
+    // res.render('index', {Articles:doc});
     }
   });
 });
 
 
 
-// Grab an article by it's ObjectId
-app.get("/articles/:id", function(req, res) {
-  // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
-  Article.findOne({ "_id": req.params.id })
-  // ..and populate all of the notes associated with it
-  .populate("note")
-  // now, execute our query
-  .exec(function(error, doc) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise, send the doc to the browser as a json object
-    else {
-      res.json(doc);
-    }
-  });
-});
+// // Grab an article by it's ObjectId
+// app.get("/articles/:id", function(req, res) {
+//   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+//   Article.findOne({ "_id": req.params.id })
+//   // ..and populate all of the notes associated with it
+//   .populate("note")
+//   // now, execute our query
+//   .exec(function(error, doc) {
+//     // Log any errors
+//     if (error) {
+//       console.log(error);
+//     }
+//     // Otherwise, send the doc to the browser as a json object
+//     else {
+//       res.json(doc);
+//     }
+//   });
+// });
 
 app.post("/saved/:id", function(req, res) {
 
@@ -159,7 +160,10 @@ app.post("/saved/:id", function(req, res) {
         }
         else {
           // Or send the document to the browser
-          res.send(doc);
+           var hbsObject = { fist5: dbFist5 };
+            return res.render("index", hbsObject);
+
+          res.json(doc);
         }
       });
     
@@ -168,35 +172,35 @@ app.post("/saved/:id", function(req, res) {
 
 
 
-// Create a new note or replace an existing note
-app.post("/articles/:id", function(req, res) {
-  // Create a new note and pass the req.body to the entry
-  var newNote = new Note(req.body);
+// // Create a new note or replace an existing note
+// app.post("/articles/:id", function(req, res) {
+//   // Create a new note and pass the req.body to the entry
+//   var newNote = new Note(req.body);
 
-  // And save the new note the db
-  newNote.save(function(error, doc) {
-    // Log any errors
-    if (error) {
-      console.log(error);
-    }
-    // Otherwise
-    else {
-      // Use the article id to find and update it's note
-      Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
-      // Execute the above query
-      .exec(function(err, doc) {
-        // Log any errors
-        if (err) {
-          console.log(err);
-        }
-        else {
-          // Or send the document to the browser
-          res.send(doc);
-        }
-      });
-    }
-  });
-});
+//   // And save the new note the db
+//   newNote.save(function(error, doc) {
+//     // Log any errors
+//     if (error) {
+//       console.log(error);
+//     }
+//     // Otherwise
+//     else {
+//       // Use the article id to find and update it's note
+//       Article.findOneAndUpdate({ "_id": req.params.id }, { "note": doc._id })
+//       // Execute the above query
+//       .exec(function(err, doc) {
+//         // Log any errors
+//         if (err) {
+//           console.log(err);
+//         }
+//         else {
+//           // Or send the document to the browser
+//           res.send(doc);
+//         }
+//       });
+//     }
+//   });
+// });
 
 
 // Listen on port 3000
